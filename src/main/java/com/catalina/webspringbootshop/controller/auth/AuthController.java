965cd49d80f87fc1c16ffdfdcc5cae2dce03f0b1
Stipo Margic <stipo@liberato.io>
2020-12-23 11:14:22 +0100

    Added registration

---
+++ src/main/java/com/catalina/webspringbootshop/controller/auth/AuthController.java
@@ -0,0 +1,47 @@
+package com.catalina.webspringbootshop.controller.auth;
+
+import com.catalina.webspringbootshop.dto.UserRegistration;
+import com.catalina.webspringbootshop.repository.UserRepository;
+import com.catalina.webspringbootshop.service.AuthenticationService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationService authenticationService;
+
+    Logger logger = LoggerFactory.getLogger(AuthController.class);
+
+    @RequestMapping(value = "/register", method = {RequestMethod.GET, RequestMethod.POST})
+    public String register(ModelMap model, HttpServletRequest req, RedirectAttributes attr,
+                           @RequestParam(value = "g-recaptcha-response", required = false) String recaptchaResp, UserRegistration userRegistration) {
+        if (StringUtils.equals(req.getMethod(), RequestMethod.GET.toString())) {
+            return "admin/auth/register";
+        }
+        if (StringUtils.equals(req.getMethod(), RequestMethod.POST.toString())) {
+            return authenticationService.doRegister(userRegistration, attr, req);
+        }
+
+        return invalidRequestResponse(attr);
+    }
+
+    private String invalidRequestResponse(RedirectAttributes attr) {
+        attr.addFlashAttribute("error", "Invalid Request Method");
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/catalina/webspringbootshop/service/AuthenticationService.java
@@ -0,0 +1,62 @@
+package com.catalina.webspringbootshop.service;
+
+import com.catalina.webspringbootshop.dto.UserRegistration;
+import com.catalina.webspringbootshop.entity.User;
+import com.catalina.webspringbootshop.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Service
+public class AuthenticationService {
+
+    private UserRepository userRepository;
+    private Logger logger = LoggerFactory.getLogger(AuthenticationService.class);
+    AuthenticationManager authManager;
+
+
+    @Autowired
+    public AuthenticationService(UserRepository userRepository, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.authManager = authenticationManager;
+    }
+
+    public String doRegister(UserRegistration userRegistration, RedirectAttributes attr, HttpServletRequest req) {
+
+        if (!userRegistration.isValidDetails()) {
+            attr.addFlashAttribute("error", "One or More Field is Missing");
+            attr.addFlashAttribute("formdata", userRegistration);
+            return "redirect:/register";
+        }
+
+        if (!userRegistration.passwordMatch()) {
+            attr.addFlashAttribute("error", "Passwords Do Not Match");
+            attr.addFlashAttribute("formdata", userRegistration);
+            return "redirect:/register";
+        }
+
+        if (userRepository.findByUsername(userRegistration.getUsername()) != null) {
+            attr.addFlashAttribute("error", "Username not available, pick another one please");
+            attr.addFlashAttribute("formdata", userRegistration);
+            return "redirect:/register";
+        }
+
+        if (userRepository.findByEmail(userRegistration.getEmail()) != null) {
+            attr.addFlashAttribute("error", "Email not available, use another one please");
+            attr.addFlashAttribute("formdata", userRegistration);
+            return "redirect:/register";
+        }
+
+        User user = userRegistration.createUserObject();
+        userRepository.save(user);
+
+        attr.addFlashAttribute("success", "Registration Successful!");
+
+        return "redirect:/login";
+    }
+}
