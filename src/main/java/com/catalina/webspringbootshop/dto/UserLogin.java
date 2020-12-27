4e84dd9cc13ddd33ed6d198e95c253c00ff45f1e
Stipo Margic <stipo@liberato.io>
2020-12-27 20:36:25 +0100

    Login feature

---
+++ src/main/java/com/catalina/webspringbootshop/dto/UserLogin.java
@@ -0,0 +1,24 @@
+package com.catalina.webspringbootshop.dto;
+
+public class UserLogin {
+    private String email, password;
+
+    public UserLogin() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
