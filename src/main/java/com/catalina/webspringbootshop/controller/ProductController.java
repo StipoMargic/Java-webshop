9583524ac6bd097da583ba7ac45998c11a07da00
Stipo Margic <stipo@liberato.io>
2020-12-29 12:26:31 +0100

    ProductService

--- src/main/java/com/catalina/webspringbootshop/controller/ProductController.java
+++ src/main/java/com/catalina/webspringbootshop/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.catalina.webspringbootshop.controller;
 
 import com.catalina.webspringbootshop.entity.Product;
+import com.catalina.webspringbootshop.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
