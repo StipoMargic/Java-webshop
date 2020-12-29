c0eb9775e09071017ee9f3b47eff645f7413efcd
Stipo Margic <stipo@liberato.io>
2020-12-29 12:06:05 +0100

    Product controller

---
+++ src/main/java/com/catalina/webspringbootshop/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.catalina.webspringbootshop.controller;
+
+import com.catalina.webspringbootshop.entity.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+    //TODO: Wishfull programing
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping(value = {"/", "/index", "/home"})
+    public String home(ModelMap model) {
+        model.addAttribute("products", getAllProducts());
+
+        return "home";
+    }
+
+    private List<Product> getAllProducts() {
+        return productService.findAllByOrderByAsc();
+    }
+
+}
