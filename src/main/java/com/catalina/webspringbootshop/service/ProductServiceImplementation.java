bec93ac12b72ea218ef1042c1fb2a8e6d08e1407
Stipo Margic <stipo@liberato.io>
2020-12-29 12:32:20 +0100

    Small product fixes

--- src/main/java/com/catalina/webspringbootshop/service/ProductServiceImplementation.java
+++ src/main/java/com/catalina/webspringbootshop/service/ProductServiceImplementation.java
@@ -3,9 +3,11 @@ package com.catalina.webspringbootshop.service;
 import com.catalina.webspringbootshop.entity.Product;
 import com.catalina.webspringbootshop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductServiceImplementation implements ProductService {
     private final ProductRepository productRepository;
 
@@ -23,7 +25,7 @@ public class ProductServiceImplementation implements ProductService {
     public void edit(int id, Product newProduct) {
         Product oldProduct = productRepository.getOne(id);
         oldProduct.setName(newProduct.getName());
-        oldProduct.setProduct_id(newProduct.getUnit_in_stock());
+        oldProduct.setUnit_in_stock(newProduct.getUnit_in_stock());
         oldProduct.setDescription(newProduct.getDescription());
         oldProduct.setPrice(newProduct.getPrice());
         save(oldProduct);
